11959c98e992c392bc5c3846f442f10c5c2e6690
Timothy Browne <timbrowne066@gmail.com>
2018-01-17 17:49:03 -0500

    controller config and root direction

---
+++ app/javascript/containers/contactsContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ContactsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="ContactsContainer">
+      <h1>Hello from contacts container</h1>
+      </div>
+    );
+  }
+}
+
+export default ContactsContainer;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,12 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+
+import ReactOnRails from 'react-on-rails';
+
+import ContactsContainer from '../containers/contactsContainer';
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+  ContactsContainer,
+});
